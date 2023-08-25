51ee1c47ec0071e5a2609308c5a850535ff8e718
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-25 08:39:45 -0300

    :test_tube: [test] cyclotron-proton-6x6

--- test/test_cyclotron.py
+++ test/test_cyclotron.py
@@ -9,3 +9,8 @@ def test_cyclotron_electron_particles_4x4():
 def test_cyclotron_proton_particles_4x4():
     proton_def = "[p, p, p, p]\n[p, 1, 1, p]\n[p, 1, p, p]\n[p, p, p, 1]\n"
     assert cyclotron("p", 4) == proton_def
+
+
+def test_cyclotron_proton_particles_6x6():
+    proton_def = "[p, p, p, p, p, p]\n[p, 1, 1, 1, 1, p]\n[p, 1, 1, 1, 1, p]\n[p, 1, 1, 1, 1, p]\n[p, 1, 1, 1, p, p]\n[p, p, p, p, p, 1]"
+    assert cyclotron("p", 6) == proton_def
