d00546b02839edf651afc9414183f417d1da5f38
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-25 11:19:22 -0300

    :sparkles: [feat] case-electron-refactor

--- cyclotron.py
+++ cyclotron.py
@@ -1,16 +1,20 @@
-def case_electron(matrix_size):
+def case_electron(particle, matrix_size):
     matrix = ""
-    first_line = "[e, " + (matrix_size - 2) * ("e" + ", ") + "e]\n"
-    other_lines = "[1" + (matrix_size - 2) * (", 1") + ", e]\n"
+    first_line = "[" + particle + ", " + (matrix_size - 2) * (particle + ", ") + particle + "]\n"
+    other_lines = "[1" + (matrix_size - 2) * (", 1") + ", " + particle + "]\n"
     matrix += first_line + (matrix_size - 1) * other_lines
     return matrix
 
 
-def case_proton(matrix_size):
+def case_proton(particle, matrix_size):
+    matrix = ""
+    # first_line = "[, " + (matrix_size - 2) * ("e" + ", ") + "e]\n"
+    # other_lines = "[1" + (matrix_size - 2) * (", 1") + ", e]\n"
+    # matrix += first_line + (matrix_size - 1) * other_lines
     return "[p, p, p, p]\n[p, 1, 1, p]\n[p, 1, p, p]\n[p, p, p, 1]\n"
 
 
-def case_neutron(matrix_size):
+def case_neutron(particle, matrix_size):
     return "[n, n, n, n]\n[1, 1, 1, 1]\n[1, 1, 1, 1]\n[1, 1, 1, 1]\n"
 
 
@@ -21,9 +25,9 @@ def case_none():
 def cyclotron(particle=None, matrix_size=4):
     matrix = ""
     case = {
-        "e": case_electron(matrix_size),
-        "p": case_proton(matrix_size),
-        "n": case_neutron(matrix_size),
+        "e": case_electron(particle, matrix_size),
+        "p": case_proton(particle, matrix_size),
+        "n": case_neutron(particle, matrix_size),
         None: case_none()
     }
     matrix += case[particle]
