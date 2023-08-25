a3032b50e382d4966ebb92126927e9a127047648
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-25 11:14:36 -0300

    :sparkles: [feat] cases-electron-done

--- cyclotron.py
+++ cyclotron.py
@@ -1,12 +1,16 @@
-def case_electron():
-    return "[e, e, e, e]\n[1, 1, 1, e]\n[1, 1, 1, e]\n[1, 1, 1, e]\n"
+def case_electron(matrix_size):
+    matrix = ""
+    first_line = "[e, " + (matrix_size - 2) * ("e" + ", ") + "e]\n"
+    other_lines = "[1" + (matrix_size - 2) * (", 1") + ", e]\n"
+    matrix += first_line + (matrix_size - 1) * other_lines
+    return matrix
 
 
-def case_proton():
+def case_proton(matrix_size):
     return "[p, p, p, p]\n[p, 1, 1, p]\n[p, 1, p, p]\n[p, p, p, 1]\n"
 
 
-def case_neutron():
+def case_neutron(matrix_size):
     return "[n, n, n, n]\n[1, 1, 1, 1]\n[1, 1, 1, 1]\n[1, 1, 1, 1]\n"
 
 
@@ -17,13 +21,13 @@ def case_none():
 def cyclotron(particle=None, matrix_size=4):
     matrix = ""
     case = {
-        "e": case_electron(),
-        "p": case_proton(),
-        "n": case_neutron(),
+        "e": case_electron(matrix_size),
+        "p": case_proton(matrix_size),
+        "n": case_neutron(matrix_size),
         None: case_none()
     }
     matrix += case[particle]
     return matrix
 
 
-print(cyclotron(None, 4))
+print(cyclotron("e", 4))
