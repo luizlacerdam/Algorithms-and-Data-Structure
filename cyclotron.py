c89e1bf1717cfece53bfca14e04ce71f8a962923
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-25 10:34:19 -0300

    :sparkles: [feat] default-matrix

--- cyclotron.py
+++ cyclotron.py
@@ -1,5 +1,14 @@
 def cyclotron(particle, matrix_size):
     matrix = ""
+    for i in range(matrix_size):
+        matrix += "["
+        for j in range(matrix_size):
+            if j == matrix_size - 1:
+                matrix += "1"
+            else:
+                matrix += "1" + ", "
+        matrix += "]"
+        matrix += "\n"
     return matrix
 
 
