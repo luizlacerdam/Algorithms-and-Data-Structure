819b137b53127fc52d90a2f5f22b0b787ce6a4a5
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-25 08:35:33 -0300

    :test_tube: [test] cyclotron-proton-4x4

--- test/test_cyclotron.py
+++ test/test_cyclotron.py
@@ -4,3 +4,8 @@ from cyclotron import cyclotron
 def test_cyclotron_electron_particles_4x4():
     electron_def = "[e, e, e, e]\n[1, 1, 1, e]\n[1, 1, 1, e]\n[1, 1, 1, e]\n"
     assert cyclotron("e", 4) == electron_def
+
+
+def test_cyclotron_proton_particles_4x4():
+    proton_def = "[p, p, p, p]\n[p, 1, 1, p]\n[p, 1, p, p]\n[p, p, p, 1]\n"
+    assert cyclotron("p", 4) == proton_def
